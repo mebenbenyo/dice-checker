16dfec00d0a0bbfcb6d903f0ed5f4771ac7c865a
mebenbenyo <117006580+mebenbenyo@users.noreply.github.com>
2023-06-24 22:46:22 -0400

    Add files via upload

---
+++ src/DiceChecker.java
@@ -0,0 +1,88 @@
+/* Description: A program to check the fairness of an 8-sided dice when rolled 
+ * in a game. It ensures that no player has an unfair advantage or disadvantage.
+ */
+
+/*
+ * This class models a an 8-sided dice and will be used to ensure the dice roll
+ * gives each game player an equal chance. 
+ */
+public class DiceChecker {
+	private final int DICE_SIZE; // Initialization deferred to constructor
+	private int[] values = null;
+	
+	/*
+	 * No-argument constructor, sets the size of the dice to 8-sides
+	 * as a default value.
+	 */
+	public DiceChecker() {
+		this(8); // default to 8 sided dice
+	}
+	
+	/*
+	 * Overloaded constructor accepts the dice face count to make
+	 * the program more flexible for dice with different number of sides.
+	 * e.g. 4-sided, 6-sided and so on. If the dice size is less than 4
+	 * then it will be set to 4 by default.
+	 */
+	public DiceChecker(int faceCount) {
+		if(faceCount < 4) {
+			faceCount = 4;
+		}
+		// (non-static constant can have initialization
+		// deferred to a constructor).
+		DICE_SIZE = faceCount;
+		values = new int[DICE_SIZE];
+	}
+	
+	/*
+	 * This method prompts the user to enter integer values between 1, i.e. min,
+	 * to the max value which is based on the DICE_SIZE constant value. After each 
+	 * value the user is asked if they would like to enter more values (y/n).
+	 * Each value is used as an index, offset by -1, in the array and for
+	 * each entry the count stored in the associated element is increased by 1.
+	 */
+	public void enterData() {
+		int input = 0;
+		int min = 1;
+		int max = DICE_SIZE;
+		String continueDataEntry = null;
+		do {
+			input = User.inputInteger("Enter dice value: ", min, max);
+			// numbers will be from 1 to DICE_SIZE, so offset by -1 as index
+			// and add 1 to the value in the indexed element.
+			// i.e. face value 1 is stored at index 0 and so on.
+			values[input - 1]++;
+			continueDataEntry = User.inputString("Enter another? (y/n): ");
+		}while(continueDataEntry.equalsIgnoreCase("y") );
+	}
+	
+	/*
+	 * Inner loop uses a for loop to traverse an array of int[] with values.
+	 * Outer loop uses a for loop to print '*' based on the number in the array
+	 *  element.
+	 */
+	public void printReport() {
+	
+		int size = DICE_SIZE;
+		for(int index = 0; index < size; index++ ) {
+			System.out.printf("%3d", (index+1));
+			
+			for(int entry = 0; entry < values[index]; entry++ ) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		
+	}
+	
+	/*
+	 * This method iterates over the values array and
+	 * places zero into each element.
+	 */
+	public void resetData() {
+		for(int index = 0; index < values.length; index++) {
+			values[index] = 0; // zero out each element
+		}
+	}
+}
